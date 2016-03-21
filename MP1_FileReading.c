4576e65377bf134683c011ff883598ba22426cc2
mjbbunag <mjbbunag@gmail.com>
2016-03-21 19:22:55 +0800

    Draw grid
    Sooooo naddraw niya na yung grid pero wala pa yung cars so di ko alam
    kung progress ba to or not lol

--- MP1_FileReading.c
+++ MP1_FileReading.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int intGrid();
+int carCoordinates();
+int drawGird();
 
 int gridSize;
-char grid[10][10];
+char carCoor[10][10];
 FILE*fptr;
 
 int main(){
@@ -12,37 +13,65 @@ int main(){
 	}
 	else{
 		intGrid();
+		drawGrid();
 	}
-
-
-
 }
 
 int intGrid(){
 	char charac;
 	int i=-1,j=0;
 	
-	//gridSize=atoi("5");
-	//printf("%d\n",gridSize);
+
 	while((charac=fgetc(fptr))!=EOF){
 		if(charac=='\n'){
+			//if newline, add new row to grid
 			i++;
 			j=0;
 		}
 		else if (i==-1){
 			gridSize=charac-'0';
+			//char->int of gridsize
 			printf("%d\n",gridSize);
 		}
 		else if(!isspace(charac)){
-			grid[i][j]=charac;
+			//append to grid(list)
+			carCoor[i][j]=charac;
 			j++;
 		}
 	}
 	for(i=0;i<6;i++){
 		for(j=0;j<4;j++){
-			printf("%c, ",grid[i][j]);
+			printf("%c, ",carCoor[i][j]);
 		}
 		printf("\n");
 	}
 
+}
+
+int drawGrid(){
+	int i,j;
+	for(i=0;i<=gridSize+1;i++){
+		for(j=0;j<=(gridSize*2)+2;j++){		
+			if(i==0 || i==gridSize+1){
+				if(j==0 || j==(gridSize*2)+2){
+					printf("+");			
+				}
+				else{
+					printf("-");
+				}
+			}
+			else{
+				if(j==0 || j==(gridSize*2)+2 && i!=gridSize/2){
+					printf("|");
+				}
+				else if(j%2==0 && j!=(gridSize*2)+2){
+					printf("*");
+				}
+				else{
+					printf(" ");
+				}
+			}
+		}
+		printf("\n");
+	}
 }
